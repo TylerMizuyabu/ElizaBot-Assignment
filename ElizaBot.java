ab9fa70da6bebf5b83fe021b776b217c4a72fbdf
TylerMizuyabu <tylermizuyabu@gmail.com>
2017-02-05 16:35:44 -0500

    Completed version handed in to professor.
    The completed version handed in as assignment 1. Not very well tested and the script containing all rules for bot is not very good and could use improvement. May get back to that.

--- ElizaBot.java
+++ ElizaBot.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,6 +42,7 @@ public class ElizaBot {
 	private HashMap<String, String> postSub;
 	private ArrayList<String> synonyms;
 	private HashMap<String, KeyDetails> keyWords;
+	private String inputSynonym = "";
 
 	public ElizaBot() {
 		this.quit = new ArrayList<String>();
@@ -53,36 +56,54 @@ public class ElizaBot {
 		return this.GREETING;
 	}
 
-	public void addPreSub(String key, String value){
-		this.preSub.put(key,value);
+	public void addPreSub(String key, String value) {
+		this.preSub.put(key, value);
 	}
 
-	public void addPostSub(String key, String value){
-		this.preSub.put(key,value);
+	public void addPostSub(String key, String value) {
+		this.postSub.put(key, value);
 	}
 
-	public void addSynonyms(String value){
+	public void addSynonyms(String value) {
 		this.synonyms.add(value);
 	}
 
-	public void addQuit(String value){
+	public void addQuit(String value) {
 		this.quit.add(value);
 	}
 
-	public void addKeyWord(String key, String value){
-
+	public void addKeyWord(String keyword, int keyValue) {
+		this.keyWords.put(keyword, new KeyDetails(keyValue));
 	}
 
-	//Method responsible for analyzing user input and returning associated assembly rules to use
-	public ArrayList<String> process(String input) {
-		//Breaks the input into words and does pre substitution
-		String[] words = input.split(" ");
+	public void addDecomp(String keyword, String decompRule) {
+		this.keyWords.get(keyword).addDecomp(decompRule);
+	}
+
+	public void addAssembly(String keyword, String decompRule, String assemblyRule) {
+		this.keyWords.get(keyword).addAssembly(decompRule, assemblyRule);
+	}
+
+	public boolean quit(String input) {
+		if (this.quit.contains(input)) {
+			System.out.println(this.quit.get(((int) (Math.random() * this.quit.size()))).toLowerCase());
+			return true;
+		}
+		return false;
+	}
+
+	public String[] convertToWords(String input) {
+		String[] words = input.replaceAll("(?!')\\p{P}", "").toLowerCase().split(" ");
 		for (int i = 0; i < words.length; i++) {
 			if (preSub.containsKey(words[i])) {
 				words[i] = preSub.get(words[i]);
 			}
+
 		}
-		//Finds keywords and sorts in descending weight
+		return Arrays.toString(words).replaceAll("\\p{P}", "").split(" ");
+	}
+
+	public ArrayList<String> sortedKeyWords(String[] words) {
 		ArrayList<String> keywords = new ArrayList<String>();
 		for (int i = 0; i < words.length; i++) {
 			if (keyWords.containsKey(words[i])) {
@@ -95,52 +116,130 @@ public class ElizaBot {
 							break;
 						} else if (j == keywords.size() - 1) {
 							keywords.add(words[i]);
+							break;
 						}
 					}
 				}
 			}
 		}
+		return keywords;
+	}
+
+	public String findDecomp(String keyWord, String input) {
+		HashMap<String, ArrayList<String>> rules = this.keyWords.get(keyWord).getRules();
+		Iterator<String> decompIterator = rules.keySet().iterator();
+		while (decompIterator.hasNext()) {
+			String decompRule = decompIterator.next();
+			// System.out.println("Decomp Rule: " + decompRule);
+			if (decompRule.contains("@")) {
+				Pattern p = Pattern.compile("@[\\w]+", Pattern.CASE_INSENSITIVE);
+				Matcher m = p.matcher(decompRule);
+				m.find();
+				String synWrd = m.group(0).substring(1); // Retrieves the word
+															// that is
+															// synonymous
+															// without the @
+				String[] allSyns = findSyns(synWrd);
+				for (int i = 0; i < allSyns.length; i++) {
+					String tempRule = m.replaceAll(allSyns[i]);
+					if (match(tempRule, input)) {
+						this.inputSynonym = allSyns[i];
+						return decompRule;
+					}
+				}
+			} else {
+				if (match(decompRule, input)) {
+					return decompRule;
+				}
+			}
+		}
+		return null;
+	}
+
+	public boolean match(String decompRegEx, String text) {
+		Pattern p = Pattern.compile(decompRegEx, Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(text);
+		return m.matches();
+	}
+
+	public String[] findSyns(String word) {
+		for (int i = 0; i < this.synonyms.size(); i++) {
+			String compWord = this.synonyms.get(i).split(" ")[0];
+			if (compWord.equalsIgnoreCase(word)) {
+				return this.synonyms.get(i).split(" ");
+			}
+		}
+		return null;
+	}
+
+	// Method responsible for analyzing user input and generate a response
+	public void process(String input) {
+		// Breaks the input into words and does pre substitution
+		String[] words = convertToWords(input);
+		input = Arrays.toString(words).replaceAll("\\p{P}", "");
+		// System.out.println(Arrays.toString(words));
+		// Finds keywords and sorts in descending weight
+		ArrayList<String> keywords = sortedKeyWords(words);
+		String decompRule = null;
 		Iterator<String> it = keywords.iterator();
 		while (it.hasNext()) {
-			HashMap<String, ArrayList<String>> rules = keyWords.get(it.next()).getRules();
-			Iterator<String> decompIt = rules.keySet().iterator();
-			while (decompIt.hasNext()) {
-				String decompRule = decompIt.next();
-				if (decompRule.contains("@")) {
-					Pattern p = Pattern.compile("@[^\\s]+", Pattern.CASE_INSENSITIVE);
-					Matcher m = p.matcher(decompRule);
-					String synWrd = m.group(0).substring(1); // Retrieves the word that is synonymous without the @
-					for (int i = 0; i < synonyms.size(); i++) {
-						if (synonyms.get(i).contains(synWrd)) {
-							String[] wrds = synonyms.get(i).split(" ");
-							for (int j = 0; j < wrds.length; j++) {
-								//Make decomp rule
-								//See if there is a match with input text
-								//If there use that decomp rule, no need to continue looking for another decomp rule
-								String tempRule = m.replaceAll(wrds[j]);
-								Pattern pRule = Pattern.compile(tempRule,Pattern.CASE_INSENSITIVE);
-								Matcher mRule = pRule.matcher(input);
-								if (mRule.matches()){
-									return rules.get(decompRule);
-								}
-							}
-						}
-					}
-
-				}else{
-					//There are no synonyms then ...
-					Pattern pRule = Pattern.compile(decompRule, Pattern.CASE_INSENSITIVE);
-					Matcher mRule = pRule.matcher(input);
-					if (mRule.matches()){
-						return rules.get(decompRule);
-					}
-					//Check if the decomp rule matches the text input
-					//If yes use this decomp rule, no need to continue looking for another decomp rule
+			String currentKey = it.next();
+			decompRule = this.findDecomp(currentKey, input);
+			if (decompRule != null) {
+				String asmblyRule = chooseAssembly(this.keyWords.get(currentKey).getRules().get(decompRule));
+				if (asmblyRule.split(" ")[0].equalsIgnoreCase("goto")) {
+					keywords.add(keywords.indexOf(currentKey) + 1, asmblyRule.split(" ")[1]);
+				} else {
+					System.out.println(reAssemble(decompRule, input, asmblyRule).toLowerCase());
+					return;
 				}
 			}
 		}
-		//Nothing matches then ...
-		return keyWords.get("xnone").getRules().get(".*");
+
+		return;
+	}
+
+	public String reAssemble(String decRule, String input, String asmblyRule) {
+		input = input.toLowerCase();
+
+		String[] splitter = decRule.trim().split("\\(\\.\\*\\)");
+		for (int i = 0; i < splitter.length; i++) {
+			if (!splitter[i].equals("")) {
+				if (!splitter[i].contains("@")) {
+					input = input.replaceFirst(splitter[i], "|");
+					//System.out.println(Arrays.toString(input.replaceFirst(splitter[i], "|").replaceFirst(inputSynonym, "|").split("\\|")));
+				} else {
+					input = input.replaceFirst(this.inputSynonym, "|"+this.inputSynonym+"|");
+				}
+			}
+		} // End of Loop
+		String[] content = input.split("\\|");
+		//System.out.println(Arrays.toString(content));
+		for (int i = 0; i < content.length; i++) {
+			content[i] = content[i].trim();
+			String[] temp = content[i].split(" ");
+			for (int j = 0; j < temp.length; j++) {
+				if(this.postSub.containsKey(temp[j])){
+					temp[j] = this.postSub.get(temp[j]);
+				}
+			}
+			content[i] = Arrays.toString(temp).replaceAll("(,|\\[|\\])", "");
+		}
+		String[] spltAssmbly = asmblyRule.split(" ");
+		for (int i = 0; i < spltAssmbly.length; i++) {
+			Pattern p = Pattern.compile("\\(\\d\\)");
+			Matcher m = p.matcher(spltAssmbly[i]);
+			if (m.find()) {
+				int x = Integer.parseInt(Character.toString(m.group(0).charAt(1))) - 1;
+				spltAssmbly[i] = m.replaceAll(content[x]);
+			}
+		}
+		return Arrays.toString(spltAssmbly).replaceAll("(,|\\[|\\])", "");
+	}
+
+	public String chooseAssembly(ArrayList<String> rules) {
+		int x = (int) (Math.random() * rules.size());
+		return rules.get(x);
 	}
 }
 
@@ -148,17 +247,19 @@ class KeyDetails {
 
 	private int rating;
 	// HashMap of the form <RegEx Decomp Rule, ArrayList<Reassembly Rules>>
-	private HashMap<String, ArrayList<String>> rules;
+	private LinkedHashMap<String, ArrayList<String>> rules;
 
 	/*
 	 * if no rating is given the default rating is 0
 	 */
 	public KeyDetails() {
 		this.rating = 0;
+		this.rules = new LinkedHashMap<String, ArrayList<String>>();
 	}
 
 	public KeyDetails(int rating) {
 		this.rating = rating;
+		this.rules = new LinkedHashMap<String, ArrayList<String>>();
 	}
 
 	/*
--- ElizaRunner.java
+++ ElizaRunner.java
@@ -1,16 +1,84 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 class ElizaRunner {
 
-  public static String inputFile = "ElizaScript";
+	public static void main(String[] args) {
+		ElizaBot bot = new ElizaBot();
+		try {
+			//File path
+			Path path = Paths.get("ElizaScript.txt");
+			BufferedReader br = new BufferedReader(new FileReader(new File(path.toString())));
+			String line = br.readLine();
+			String lastKeyWord = "";
+			String lastDecompRule ="";
+			while (line != null) {
+				line=line.trim();
+				String head = line.substring(0, line.indexOf(" "));
+				String strippedString;
+				switch (head) {
+				case "quit:":
+					bot.addQuit(line.substring(line.indexOf(" ")+1));
+					//System.out.println(line.substring(line.indexOf(" ")+1));
+					break;
+				case "pre:":
+					strippedString = line.substring(line.indexOf(" ")+1);
+					bot.addPreSub(strippedString.substring(0,strippedString.indexOf(" ")), strippedString.substring(strippedString.indexOf(" ")+1));
+					//System.out.println(strippedString.substring(0,strippedString.indexOf(" ")) + " " + strippedString.substring(strippedString.indexOf(" ")+1));
+					break;
+				case "post:":
+					strippedString = line.substring(line.indexOf(" ")+1);
+					bot.addPostSub(strippedString.substring(0,strippedString.indexOf(" ")), strippedString.substring(strippedString.indexOf(" ")+1));
+					//System.out.println(strippedString.substring(0,strippedString.indexOf(" ")) + " " + strippedString.substring(strippedString.indexOf(" ")+1));
+					break;
+				case "synon:":
+					bot.addSynonyms(line.substring(line.indexOf(" ")+1));
+					//System.out.println(line.substring(line.indexOf(" ")+1));
+					break;
+				case "key:":
+					String[] keydetails = line.substring(line.indexOf(" ") + 1).split(" ");
+					int rating = 0;
+					if (keydetails.length > 1){
+						rating=Integer.parseInt(keydetails[1]);
+					}
+					bot.addKeyWord(keydetails[0], rating);
+					lastKeyWord = keydetails[0];
+					//System.out.println(lastKeyWord + " " + rating);
+					break;
+				case "decomp:":
+					lastDecompRule = line.substring(line.indexOf(" ") + 1);
+					bot.addDecomp(lastKeyWord, lastDecompRule);
+					//System.out.println(lastDecompRule);
+					break;
+				case "reasmb:":
+					bot.addAssembly(lastKeyWord, lastDecompRule, line.substring(line.indexOf(" ")+1));
+					//System.out.println(line.substring(line.indexOf(" ")+1));
+					break;
+				default:
+					break;
+				}
 
-  public static void main(String[]args){
-    try{
-      BufferedReader br = new BufferedReader(new FileReader(new File(inputFile)));
-      String line = br.readLine();
-      
-    }catch(IOException e){
-      e.printStackTrace();
-    }
-  }
+				line = br.readLine();
+			}
+			System.out.println(bot.greet());
+			br = new BufferedReader(new InputStreamReader(System.in));
+			line = br.readLine();
+			while(!bot.quit(line)){
+				//bot processes input
+				//generates responce
+				bot.process(line);
+
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
